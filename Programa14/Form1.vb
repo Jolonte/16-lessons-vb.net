547153445b0ce21844c03ac13f902c163205c893
Vinícius <66970818+Jolonte@users.noreply.github.com>
2021-07-25 15:00:23 -0300

    Add files via upload

---
+++ Programa14/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dolar, real As Double
+
+        dolar = TextBox1.Text
+        real = TextBox2.Text
+
+        TextBox3.Text = dolar * real
+    End Sub
+End Class
